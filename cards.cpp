85e99916606dda8eee1f6423ce570b63cbf269ee
Lizette <lbc@ucsb.edu>
2022-02-13 21:59:14 -0800

    worked on functions

--- cards.cpp
+++ cards.cpp
@@ -14,3 +14,137 @@ CardBST::CardBST() : root(0) { }
 CardBST::~CardBST() {
     clear(root);
 }
+
+//destructor helper
+void CardBST::clear(Node *n) {
+    if (n) {
+	clear(n->left);
+	clear(n->right);
+	delete n;
+    }
+}
+
+// overloaded == operator
+bool operator==(const Card& c1, const Card& c2) {
+    if ( c1.getSuit() == c2.getSuit() && c1.getNumber() == c2.getNumber() ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// overloaded < operator
+// note: hearts > spades > diamonds > clubs
+bool operator<(const Card& c1, const Card& c2) {
+    if (c1 == c2) {
+        return false;
+    }
+    else if (c1.getSuit() == 'c') {
+        return true;
+    }
+    else if (c1.getSuit() == 'd') {
+        if (c2.getSuit() == 's' || c2.getSuit() == 'h') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+// overloaded > operator
+// note: hearts > spades > diamonds > clubs
+bool operator>(const Card& c1, const Card& c2) {
+    if (c1 == c2) {
+        return false;
+    }
+    else if (c1.getSuit() == 'h') {
+        return true;
+    }
+    else if (c1.getSuit() == 's') {
+        if (c2.getSuit() == 'd' || c2.getSuit() == 'c') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (c1.getSuit() == 'd') {
+        if (c2.getSuit() == 'c') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return 0;
+}
+
+// insert value in tree; return false if duplicate
+bool CardBST::insertCard(char suit, int number) {
+    // handle special case of empty tree first
+    if (!root) {
+	root = new Node(suit, number);
+	return true;
+    }
+    // otherwise use recursive helper
+    return insertCard(suit, number, root);
+}
+
+// recursive helper for insert (assumes n is never 0)
+bool CardBST::insertCard(char suit, int number, Node *n) {
+    Card iCard {suit, number};
+    if (iCard == n->newCard) {
+	    return false;
+    }
+    if (iCard < n->newCard) {
+	    if (n->left)
+	        return insertCard(suit, number, n->left);
+	    else {
+	        n->left = new Node(suit, number);
+	        n->left->parent = n;
+	        return true;
+	    }
+    }
+    else {
+	    if (n->right)
+	        return insertCard(suit, number, n->right);
+	    else {
+	        n->right = new Node(suit, number);
+	        n->right->parent = n;
+	        return true;
+	    }
+    }
+}
+
+// (DONE) returns the node for a given value or NULL if none exists
+CardBST::Node* CardBST::getNodeFor(char suit, int number, Node* n) const{
+    if (n == nullptr) {
+        return nullptr;
+    }
+    Card iCard {suit, number};
+    if (n->newCard == iCard) {
+        return n;
+    }
+    if (iCard < n->newCard) {
+        return getNodeFor(suit, number, n->left);
+    }
+    else if (iCard > n->newCard) {
+        return getNodeFor(suit, number, n->right);
+    }
+    return 0;
+}
+
+// (DONE) returns true if value is in the tree; false if not
+bool CardBST::containsCard(char suit, int number) const {
+    if (getNodeFor(suit, number, root)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- cards.h
+++ cards.h
@@ -17,27 +17,31 @@ class CardBST {
         CardBST();      // constructor
         ~CardBST();     // destructor
 
-        bool insertCard(int value);             // TODO insert value; return false if duplicate
-        bool containsCard(int value) const;     // TODO true if value is in tree
+        friend bool operator==(const Card& c1, const Card& c2);
+        friend bool operator<(const Card& c1, const Card& c2);
+        friend bool operator>(const Card& c1, const Card& c2);
+
+        bool insertCard(char suit, int number);             // insert value; return false if duplicate
+        bool containsCard(char suit, int number) const;     // true if value is in tree
         int getSuccessor(int value) const;      // TODO returns the successor value of the given value or 0 if there is none
+        int getPredecessor(int value) const;      // TODO returns the predecessor  value of the given value or 0 if there is none
         bool removeCard(int value);             // TODO deletes the Node containing the given value from the tree
 
     private:
         struct Node {
             // declare card
             Card newCard;
-	        char suit;
-            int number;
 	        Node *left, *right, * parent;
             //useful constructor
             //nCard(n, s)
             Node(char su = '0', int num = 0) : newCard(su,num), left(0), right(0), parent(0) { }
         };
 
-        Node* getNodeFor(int value, Node* n) const; // TODO returns the node for a given value or NULL if none exists
-        void clear(Node *n); // TODO for destructor
-        bool insert(int value, Node *n); // TODO note overloading names for simplicity
+        Node* getNodeFor(char suit, int number, Node* n) const; // returns the node for a given value or NULL if none exists
+        void clear(Node *n); // for destructor
+        bool insertCard(char suit, int number, Node *n); // note: overloading names for simplicity
         Node* getSuccessorNode(int value) const;   // TODO returns the Node containing the successor of the given value
+        Node* getPredecessorNode(int value) const;   // TODO returns the Node containing the predecessor of the given value
 
         Node *root;
 
