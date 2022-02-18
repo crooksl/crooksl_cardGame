f389fc729a2709c8be0ce39648ace9f9c7b85035
Lizette <lbc@ucsb.edu>
2022-02-18 14:52:16 -0800

    wrote and tested code for reading and inserting cards into aBST and bBST

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <string>
 
 using namespace std;
-#include "cards.cpp"
+#include <cstring>
+#include "cards.h"
 
 int main(int argv, char** argc){
   if(argv < 3){
@@ -21,22 +22,85 @@ int main(int argv, char** argc){
   }
 
   CardBST alice;
-  // read alice file
   while (getline (cardFile1, line) && (line.length() > 0)){
+
+    // can automatically convert to char
     char aSuit;
-    // (for char) can automatically convert to char
+    aSuit = line[0];
+
+    // check the string to see if it contains char, else convert to int (check for 10!)
     int aNumber;
-    // (for number) check the string to see if it contains char, else convert to int
+    // a = 0, j = 11, q = 12, k = 13
+    if (isalpha(line[2])) {
+      if (line[2] == 'a') {
+        aNumber = 0;
+      }
+      else if (line[2] == 'j') {
+        aNumber = 11;
+      }
+      else if (line[2] == 'q') {
+        aNumber = 12;
+      }
+      else {
+        aNumber = 13;
+      }
+    }
+    else {
+      if ("10" == line.substr(2, line.length() - 2)) {
+        aNumber = 10;
+      }
+      else {
+        // have to do num - 48 otherwise it gives ascii position!
+        aNumber = line[2] - 48;
+      }
+    }
+
+    //insert card to alice
+    alice.insertCard(aSuit, aNumber);
+
   }
   cardFile1.close();
 
   CardBST bob;
-  // read bob file
   while (getline (cardFile2, line) && (line.length() > 0)){
+
+    char aSuit;
+    aSuit = line[0];
+
+    int aNumber;
+    // a = 0, j = 11, q = 12, k = 13
+    if (isalpha(line[2])) {
+      if (line[2] == 'a') {
+        aNumber = 0;
+      }
+      else if (line[2] == 'j') {
+        aNumber = 11;
+      }
+      else if (line[2] == 'q') {
+        aNumber = 12;
+      }
+      else {
+        aNumber = 13;
+      }
+    }
+    else {
+      if ("10" == line.substr(2, line.length() - 2)) {
+        aNumber = 10;
+      }
+      else {
+        aNumber = line[2] - 48;
+      }
+    }
+    
+    bob.insertCard(aSuit, aNumber);
 
   }
   cardFile2.close();
   
+
+
+
+
   
   return 0;
 }
